12dd7dc44ea6333d44c20dc7ee8102833d836bed
Md. Moinul Hassan <moinulh.msn@gmail.com>
2022-12-03 21:27:17 +0600

    edit

--- src/components/AdvertisedSection/AdvertisedSection.js
+++ src/components/AdvertisedSection/AdvertisedSection.js
@@ -10,7 +10,7 @@ const AdvertisedSection = ({advertisedProducts, bookingProduct, setBookingProduc
         return <Loading/>
     }
     return (
-        <section className='my-10'>
+        <section className='my-10 mx-5'>
         <h2 className='text-3xl text-start font-bold mb-5 ml-7'> Advertised Items</h2>
             <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {advertisedProducts.map(product => <ProductCard key={product._id} setBookingProduct={setBookingProduct} product={product}/>)}
